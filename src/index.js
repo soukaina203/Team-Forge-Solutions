02a05e71bd0e8ecc03376547e63d94d488aa2297
soukaina203 <soukaina9mourabit@gmail.com>
2023-11-27 19:57:23 +0000

    HashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter   as Router, Route, Routes } from 'react-router-dom';
 
 import reportWebVitals from './reportWebVitals';
 import Welcome from './component/Welcome';
